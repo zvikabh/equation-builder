5bae4d984151f27f84402b93daa1ac6d7e5cb66a
Zvika Ben-Haim <zvikabh@gmail.com>
2023-05-02 18:25:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function getRandomInt(min, max) {
+  // The maximum is exclusive and the minimum is inclusive.
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function placeCoefficient(coef, id, dontWriteOnes) {
+  if (coef >= 0) {
+    if (dontWriteOnes && coef == 1) {
+      $('#' + id).text('');
+    } else {
+      $('#' + id).text(coef);
+    }
+  } else {
+    $('#pre-' + id).html('&minus;');
+    if (dontWriteOnes && coef == -1) {
+      $('#' + id).text('');
+    } else {
+      $('#' + id).text(-coef);
+    }
+  }
+}
+
+function isGoodChoice(a) {
+  for (const [key, value] of Object.entries(a)) {
+    if (value == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+$(document).ready(() => {
+  var x = getRandomInt(-10, 11);
+  var y = getRandomInt(-10, 11);
+  var a = {'a11': 0, 'a12': 0, 'a21': 0, 'a22': 0};
+  do {
+    for (const [key, value] of Object.entries(a)) {
+      a[key] = getRandomInt(-7, 8);
+    }
+  } while (!isGoodChoice(a));
+  var b1 = a.a11*x + a.a12*y;
+  var b2 = a.a21*x + a.a22*y;
+  for (const [key, value] of Object.entries(a)) {
+    placeCoefficient(value, key, true);
+  }
+  placeCoefficient(b1, 'b1', false);
+  placeCoefficient(b2, 'b2', false);
+});
